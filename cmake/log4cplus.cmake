7c421dc2b02f3a07cc1de6aa0c3afbf5cdf8a83e
R. Douglas Barbieri <doug@dooglio.net>
2013-11-08 16:32:05 -0800

    Now handles the detection of required libraries a little better.

--- cmake/log4cplus.cmake
+++ cmake/log4cplus.cmake
@@ -1,9 +1,9 @@
-find_file( log4cplus_INCLUDE_FILE log4cplus/configurator.h )
-if( ${log4cplus_INCLUDE_FILE} STREQUAL "log4cplus_INCLUDE_FILE-NOTFOUND" )
-	message( FATAL_ERROR
+find_path( log4cplus_INCLUDE_PATH log4cplus )
+if( ${log4cplus_INCLUDE_PATH} STREQUAL "log4cplus_INCLUDE_PATH-NOTFOUND" )
+	message( FATAL_ERROR "Cannot find log4cplus header path." )
 endif()
 
 find_library( log4cplus_LIBRARY log4cplus )
-if( NOT log4cplus_FOUND )
+if( ${log4cplus_LIBRARY} STREQUAL "log4cplus_LIBRARY-NOTFOUND" )
 	message( FATAL_ERROR "Cannot find the log4cplus library. Make sure it is installed and try again." )
 endif()
