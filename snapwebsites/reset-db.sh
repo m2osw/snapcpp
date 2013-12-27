7c736455b9f3a724c9be1561f24b69fbccd18528
R. Douglas Barbieri <doug@m2osw.com>
2013-12-27 11:45:09 -0800

    Now installs layouts.
    Also added a general reset script, and moved conf files into the conf
    subdir under /usr/share/snapwebsites.

--- snapwebsites/reset-db.sh
+++ snapwebsites/reset-db.sh
@@ -1,8 +1,8 @@
 #!/bin/sh
-../BUILD/snapwebsites/src/snapdb --drop-tables
-../BUILD/snapwebsites/src/snaplayout layouts/bare-body-parser.xsl
-../BUILD/snapwebsites/src/snaplayout layouts/bare-theme-parser.xsl
-../BUILD/snapwebsites/src/snaplayout layouts/white-body-parser.xsl
-../BUILD/snapwebsites/src/snaplayout layouts/white-theme-parser.xsl
-../BUILD/snapwebsites/src/snapserver -d -c src/snapserver.conf --add-host
-# ../BUILD/snapwebsites/src/snapserver -d --add-host halk
+snapdb --drop-tables
+snaplayout layouts/bare-body-parser.xsl
+snaplayout layouts/bare-theme-parser.xsl
+snaplayout layouts/white-body-parser.xsl
+snaplayout layouts/white-theme-parser.xsl
+snapserver -d -c /etc/snapserver.conf --add-host
+# snapserver -d --add-host halk
