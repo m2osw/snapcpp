3dc48907c9bd4f6fc1825a3a033aaff594831bd3
R. Douglas Barbieri <doug@dooglio.net>
2014-01-20 14:11:51 -0800

    Added more code to obtain the parent of the cell.
    This is broken, work in progress, to be finished after I merge the
    experimental branch back in which adds parent() support to
    libQtCassandra objects (right now, the parent nodes are all private).

--- cassview/src/TableModel.cpp
+++ cassview/src/TableModel.cpp
@@ -1,23 +1,63 @@
 #include "TableModel.h"
 
+#include <iostream>
+
 using namespace QtCassandra;
 
 void TableModel::setTable( QSharedPointer<QCassandraTable> t )
 {
 	f_table = t;
-    reset();
 
     QCassandraRowPredicate rowp;
     //rowp.setStartRowName("");
     //rowp.setEndRowName("");
     //rowp.setCount(10); // 100 is the default
     f_table->readRows( rowp );
+
+    reset();
 }
 
 
 Qt::ItemFlags TableModel::flags( const QModelIndex & /*idx*/ ) const
 {
-	return Qt::ItemIsEnabled | Qt::ItemIsSelectable;
+    return Qt::ItemIsEnabled | Qt::ItemIsSelectable ; //| Qt::ItemIsEditable;
+}
+
+
+QVariant TableModel::headerData( int section, Qt::Orientation orientation, int role ) const
+{
+	if( role != Qt::DisplayRole )
+	{
+        return QVariant();
+	}
+
+    if( !f_table )
+    {
+        return QVariant();
+    }
+
+    const auto& rows( f_table->rows() );
+    if( rows.size() == 0 )
+    {
+        return QVariant();
+    }
+
+    if( orientation == Qt::Horizontal )
+	{
+        const auto& row( *(rows.begin()) );
+        Q_ASSERT(row);
+
+        const auto& cell( *(row->cells().begin() + section) );
+        return cell->columnName();
+    }
+
+#if 0
+    const auto& row( *(rows.begin() + section) );
+    Q_ASSERT(row);
+
+    return row->rowName();
+#endif
+	return QVariant();
 }
 
 
@@ -33,24 +73,40 @@ QVariant TableModel::data( const QModelIndex & idx, int role ) const
         return QVariant();
     }
 
-    const QCassandraRows& rows = f_table->rows();
-    const QString row_name( (rows.begin()+idx.row()).value()->rowName() );
-    return row_name;
+    if( idx.parent().isValid() )
+	{
+		const auto& rows( f_table->rows() );
+		const auto& row( *(rows.begin() + idx.row()) );
+		Q_ASSERT(row);
+
+		const auto cells( row->cells() );
+		if( idx.column() < cells.size() )
+		{
+			const auto& iter( cells.begin() + idx.column() );
+			Q_ASSERT(iter != cells.end());
+			const auto& cell( *iter );
+			return cell->value().stringValue();
+		}
+
+		return QVariant();
+	}
+
+	const auto& rows = f_table->rows();
+	const QString row_name( (rows.begin()+idx.row()).value()->rowName() );
+	return row_name;
 }
 
 
-QVariant TableModel::headerData( int /*section*/, Qt::Orientation /*orientation*/, int /*role*/ ) const
-{
-	// TODO
-	return "Row Name";
-}
-
-
-int TableModel::rowCount( const QModelIndex & /*parent*/ ) const
+int TableModel::rowCount( const QModelIndex &prnt ) const
 {
     if( !f_table )
     {
         return 0;
+    }
+
+    if( prnt.isValid() )
+    {
+        return 1;
     }
 
     const QCassandraRows& rows = f_table->rows();
@@ -58,3 +114,59 @@ int TableModel::rowCount( const QModelIndex & /*parent*/ ) const
 }
 
 
+int TableModel::columnCount( const QModelIndex &prnt ) const
+{
+    if( !f_table )
+    {
+        return 0;
+    }
+
+    if( !prnt.isValid() )
+    {
+        return 1;
+    }
+
+    const auto& rows( f_table->rows() );
+    if( rows.size() == 0 )
+    {
+        return 0;
+    }
+
+    const auto& row( *(rows.begin()) );
+    Q_ASSERT(row);
+    return row->cells().size();
+}
+
+
+QModelIndex TableModel::index( int row, int column, const QModelIndex &prnt ) const
+{
+    const auto& the_row(( f_table->rows().begin()+row ).value());
+    //
+    if( prnt.isValid() )
+    {
+        const auto& the_column(( the_row->cells().begin()+column  ).value());
+        return createIndex( row, column, the_column.data() );
+    }
+
+    return createIndex( row, 0, the_row.data() );
+}
+
+
+QModelIndex TableModel::parent( const QModelIndex &idx ) const
+{
+	if( !idx.isValid() )
+	{
+		return QModelIndex();
+	}
+
+    if( idx.internalPointer() == 0 )
+	{
+        return QModelIndex();
+	}
+
+	QCassandraCell* cell( static_cast<QCassandraCell*>(idx.internalPointer()) );
+    return createIndex( idx.row(), 0, cell->parent().data() );
+}
+
+
+// vim: ts=4 sw=4 noet
--- cassview/src/TableModel.h
+++ cassview/src/TableModel.h
@@ -6,7 +6,7 @@
 #include <memory>
 
 class TableModel
-	: public QAbstractListModel
+    : public QAbstractItemModel
 {
 	Q_OBJECT
 
@@ -15,13 +15,25 @@ class TableModel
 
         void setTable( QSharedPointer<QtCassandra::QCassandraTable> t );
 
+        // Read only access
+        //
 		Qt::ItemFlags	flags 		( const QModelIndex & index ) const;
-		QVariant		data 		( const QModelIndex & index, int role = Qt::DisplayRole ) const;
-		QVariant		headerData	( int section, Qt::Orientation orientation, int role = Qt::DisplayRole ) const;
-		int 			rowCount   	( const QModelIndex & parent = QModelIndex() ) const;
+        QVariant		data 		( const QModelIndex & index, int role = Qt::DisplayRole ) const;
+        QVariant		headerData	( int section, Qt::Orientation orientation, int role = Qt::DisplayRole ) const;
+        int 			rowCount   	( const QModelIndex & parent = QModelIndex() ) const;
+
+        // Write access
+        //
+        //bool 		    setData     ( const QModelIndex & index, const QVariant & value, int role = Qt::EditRole );
+
+        // Required for items with columns.
+        //
+        int         columnCount( const QModelIndex & parent = QModelIndex() ) const;
+        QModelIndex index ( int row, int column, const QModelIndex & parent = QModelIndex() ) const;
+        QModelIndex parent ( const QModelIndex & index ) const;
 
 	private:
         QSharedPointer<QtCassandra::QCassandraTable>	f_table;
 };
 
-
+// vim: ts=4 sw=4 noet
