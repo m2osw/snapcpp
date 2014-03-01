7fb5a6ad86f7f7c62d15b01ab196303912782f23
R. Douglas Barbieri <doug@dooglio.net>
2014-03-01 08:58:16 -0800

    Preparing for release.

--- snapwebsites/run-server.sh
+++ snapwebsites/run-server.sh
@@ -1,4 +1,13 @@
 #!/bin/sh
 #
-./src/snapserver -d --no-log -c my_server.conf
+SNAPINIT=/usr/bin/snapinit
+SNAPFLAGS="--detach --binary_path ./src --logfile snapinit.log -c my_server.conf --all"
+
+if [ -z "$1" ]
+then
+	echo "usage $0 [start|stop|restart]"
+	exit 1
+fi
+
+./src/snapinit ${SNAPFLAGS} $1
 
