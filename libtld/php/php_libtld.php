c2729d8d4004e24799431f7626c3be90eaa589aa
AlexisWilke <alexis@m2osw.com>
2014-05-04 19:38:57 -0700

    Added a README.txt for version 1.4.6 (quite specific to that version).
    Added the BRAND category to the PHP declarations.
    Added the copyright notice to the PHP declarations file.

--- libtld/php/php_libtld.php
+++ libtld/php/php_libtld.php
@@ -1,7 +1,30 @@
 <?php
+/* TLD library -- PHP declarations
+ * Copyright (C) 2013-2014  Made to Order Software Corp.
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a
+ * copy of this software and associated documentation files (the
+ * "Software"), to deal in the Software without restriction, including
+ * without limitation the rights to use, copy, modify, merge, publish,
+ * distribute, sublicense, and/or sell copies of the Software, and to
+ * permit persons to whom the Software is furnished to do so, subject to
+ * the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included
+ * in all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS
+ * OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF
+ * MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT.
+ * IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY
+ * CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT,
+ * TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE
+ * SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+ */
+
 /*
  * I'm not too sure, at this time, how to add those declarations in the code
- * so it's writte in a .php that you can include.
+ * so it's writte in a .php that you can simply include.
  */
 
 define('TLD_CATEGORY_INTERNATIONAL', 0);
@@ -12,7 +35,8 @@ define('TLD_CATEGORY_REGION', 4);
 define('TLD_CATEGORY_TECHNICAL', 5);
 define('TLD_CATEGORY_COUNTRY', 6);
 define('TLD_CATEGORY_ENTREPRENEURIAL', 7);
-define('TLD_CATEGORY_UNDEFINED', 8);
+define('TLD_CATEGORY_BRAND', 8);
+define('TLD_CATEGORY_UNDEFINED', 9);
 
 define('TLD_STATUS_VALID', 0);
 define('TLD_STATUS_PROPOSED', 1);
