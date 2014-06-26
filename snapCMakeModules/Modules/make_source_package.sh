2c0436dede89c69a5aab10d0b74c5f6354e7bcda
R. Douglas Barbieri <doug@dooglio.net>
2014-06-25 21:47:55 -0700

    Check return values.

--- snapCMakeModules/Modules/make_source_package.sh
+++ snapCMakeModules/Modules/make_source_package.sh
@@ -9,16 +9,22 @@
 
 # Handle command line
 #
-if [ -n "$1" ]
+if [ -z "$1" ]
 then
 	DIST=saucy
 else
 	DIST=$1
 fi
 
+if [ ! -e debian/changelog ]
+then
+	echo "No debian changelog found!"
+	exit 1
+fi
+
 # Use the build server email and name. This will appear in the changelog.
 #
-export NAME=\"Build Server\" 
+export NAME="Build Server" 
 export DEBEMAIL=build@m2osw.com
 
 # Get the current version from the change log, get rid of everything past the tilde "~",
@@ -29,10 +35,17 @@ INCVER=`echo ${CURVER} | perl -pe 's/^((\d+\.)*)(\d+)(.*)$/$1.($3+1).$4/e'`
 
 # Write a new changelog entry, but using "~dist".
 #
-dch --newversion ${INCVER}~${DIST} --urgency high --distribution ${DIST} Nightly build.
+if ! dch --newversion ${INCVER}~${DIST} --urgency high --distribution ${DIST} Nightly build.
+then
+	echo "Error running dch! Aborting..."
+	exit 1;
+fi
 
 # Build the source package itself. The output will be placed in the parent directory
 # of the CWD.
 #
 debuild -S -sa
 
+exit 0
+
+# vim: ts=4 sw=4 et
