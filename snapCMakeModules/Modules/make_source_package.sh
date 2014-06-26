1ce6a33ab08dbb07fae08483fd213b8effe87a06
R. Douglas Barbieri <doug@dooglio.net>
2014-06-25 20:29:04 -0700

    Added make_source_package.sh.
    This script creates a source package for a Debian project.

---
+++ snapCMakeModules/Modules/make_source_package.sh
@@ -0,0 +1,38 @@
+#!/bin/bash
+# This script creates a source package for the project in the current directory.
+# In order for this to work properly, there has to be a debian folder present,
+# with proper files present.
+#
+# The parameter to the script is the platform on which to build (i.e. saucy, trusty, etc).
+# The default is "saucy."
+#
+
+# Handle command line
+#
+if [ -n "$1" ]
+then
+	DIST=saucy
+else
+	DIST=$1
+fi
+
+# Use the build server email and name. This will appear in the changelog.
+#
+export NAME=\"Build Server\" 
+export DEBEMAIL=build@m2osw.com
+
+# Get the current version from the change log, get rid of everything past the tilde "~",
+# and increment the version number.
+#
+CURVER=`dpkg-parsechangelog --show-field Version | sed 's/~.*$//'`
+INCVER=`echo ${CURVER} | perl -pe 's/^((\d+\.)*)(\d+)(.*)$/$1.($3+1).$4/e'`
+
+# Write a new changelog entry, but using "~dist".
+#
+dch --newversion ${INCVER}~${DIST} --urgency high --distribution ${DIST} Nightly build.
+
+# Build the source package itself. The output will be placed in the parent directory
+# of the CWD.
+#
+debuild -S -sa
+
