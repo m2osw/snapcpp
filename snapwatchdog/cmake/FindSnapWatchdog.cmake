459f77e7bbd696b57a5e40837f4531c47c40defe
R. Douglas Barbieri <doug@dooglio.net>
2014-10-23 17:40:40 -0700

    Added new cmake module.

--- snapwatchdog/cmake/FindSnapWatchdog.cmake
+++ snapwatchdog/cmake/FindSnapWatchdog.cmake
@@ -2,32 +2,25 @@
 #
 # Once done this will define
 #
-# SNAPWEBSITES_FOUND        - System has SnapWebsites
-# SNAPWEBSITES_INCLUDE_DIR  - The SnapWebsites include directories
-# SNAPWEBSITES_LIBRARY      - The libraries needed to use SnapWebsites (none)
-# SNAPWEBSITES_DEFINITIONS  - Compiler switches required for using SnapWebsites (none)
+# SNAPWATCHDOG_FOUND        - System has SnapWebsites
+# SNAPWATCHDOG_INCLUDE_DIR  - The SnapWebsites include directories
+# SNAPWATCHDOG_LIBRARY      - The libraries needed to use SnapWebsites (none)
+# SNAPWATCHDOG_DEFINITIONS  - Compiler switches required for using SnapWebsites (none)
 
-find_path( SNAPWEBSITES_INCLUDE_DIR snapwebsites/snap_version.h
-		   PATHS $ENV{SNAPWEBSITES_INCLUDE_DIR}
-		   PATH_SUFFIXES snapwebsites
+find_path( SNAPWATCHDOG_INCLUDE_DIR snapwatchdog/snapwatchdog.h
+		   PATHS $ENV{SNAPWATCHDOG_INCLUDE_DIR}
+		   PATH_SUFFIXES snapwatchdog
 		 )
-find_library( SNAPWEBSITES_LIBRARY snapwebsites
-			PATHS $ENV{SNAPWEBSITES_LIBRARY}
+find_library( SNAPWATCHDOG_LIBRARY snapwatchdog
+			PATHS $ENV{SNAPWATCHDOG_LIBRARY}
 		 )
-mark_as_advanced( SNAPWEBSITES_INCLUDE_DIR SNAPWEBSITES_LIBRARY )
+mark_as_advanced( SNAPWATCHDOG_INCLUDE_DIR SNAPWATCHDOG_LIBRARY )
 
-set( SNAPWEBSITES_INCLUDE_DIRS ${SNAPWEBSITES_INCLUDE_DIR} ${SNAPWEBSITES_INCLUDE_DIR}/snapwebsites )
-set( SNAPWEBSITES_LIBRARIES    ${SNAPWEBSITES_LIBRARY}     )
+set( SNAPWATCHDOG_INCLUDE_DIRS ${SNAPWATCHDOG_INCLUDE_DIR} ${SNAPWATCHDOG_INCLUDE_DIR}/snapwatchdog )
+set( SNAPWATCHDOG_LIBRARIES    ${SNAPWATCHDOG_LIBRARY}     )
 
 include( FindPackageHandleStandardArgs )
-# handle the QUIETLY and REQUIRED arguments and set SNAPWEBSITES_FOUND to TRUE
+# handle the QUIETLY and REQUIRED arguments and set SNAPWATCHDOG_FOUND to TRUE
 # if all listed variables are TRUE
-find_package_handle_standard_args( SnapWebsites DEFAULT_MSG SNAPWEBSITES_INCLUDE_DIR SNAPWEBSITES_LIBRARY )
+find_package_handle_standard_args( SnapWebsites DEFAULT_MSG SNAPWATCHDOG_INCLUDE_DIR SNAPWATCHDOG_LIBRARY )
 
-# Make sure default DTD files are pointed to...
-#
-set( DTD_SOURCE_PATH /usr/share/snapwebsites/dtd CACHE PATH "Default DTD source files." )
-#
-include( SnapJsLint    )
-include( SnapXmlLint   )
-include( SnapZipLayout )
