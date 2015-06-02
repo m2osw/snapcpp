30411c777cc697f87fb72c3a26077b521ebff383
AlexisWilke <alexis@m2osw.com>
2015-06-01 23:01:18 -0700

    SNAP-42: Made sure to update the dates so the new javascript files are properly taken in account.

--- snapwebsites/plugins/editor/editor.cpp
+++ snapwebsites/plugins/editor/editor.cpp
@@ -178,7 +178,7 @@ int64_t editor::do_update(int64_t last_updated)
 {
     SNAP_PLUGIN_UPDATE_INIT();
 
-    SNAP_PLUGIN_UPDATE(2015, 6, 1, 17, 33, 0, content_update);
+    SNAP_PLUGIN_UPDATE(2015, 6, 1, 22, 39, 0, content_update);
 
     SNAP_PLUGIN_UPDATE_EXIT();
 }
