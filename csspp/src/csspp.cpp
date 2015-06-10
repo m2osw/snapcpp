86ed75a49989d92d81a34b3a7d7ddbfecf968634
AlexisWilke <alexis@m2osw.com>
2015-06-09 23:38:41 -0700

    SNAP-187: Actually added the binary shell so we can create a binary package immediately (Albeit not useful yet).
    Made the necessary updates so we can create the packages.

---
+++ csspp/src/csspp.cpp
@@ -0,0 +1,53 @@
+// CSS Preprocessor
+// Copyright (C) 2015  Made to Order Software Corp.
+//
+// This program is free software; you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation; either version 2 of the License, or
+// (at your option) any later version.
+//
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with this program; if not, write to the Free Software
+// Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
+
+/** \file
+ * \brief Implementation of the CSS Preprocessor command line tool.
+ *
+ */
+
+//#include "csspp/lexer.h"
+//
+//#include "csspp/error.h"
+//#include "csspp/exceptions.h"
+//#include "csspp/unicode_range.h"
+//
+//#include <cmath>
+#include <iostream>
+
+namespace csspp
+{
+
+
+} // namespace csspp
+
+int main(int argc, char *argv[])
+{
+    static_cast<void>(argc);
+    static_cast<void>(argv);
+    std::cerr << "not implemented yet.\n";
+    return 0;
+}
+
+// Local Variables:
+// mode: cpp
+// indent-tabs-mode: nil
+// c-basic-offset: 4
+// tab-width: 4
+// End:
+
+// vim: ts=4 sw=4 et
