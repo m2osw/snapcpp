f824fa8e1fd604992d715eec8361eb0fe082378d
AlexisWilke <alexis@m2osw.com>
2015-06-09 23:49:31 -0700

    SNAP-187: Fixed 3 references to other projects.

--- csspp/cmake/FindCatch.cmake
+++ csspp/cmake/FindCatch.cmake
@@ -6,22 +6,21 @@
 # CATCH_FOUND, If false, do not try to use Catch.
 #
 # License:
-#      Zipios++ - a small C++ library that provides easy access to .zip files.
-#      Copyright (C) 2015  Made to Order Software Corporation
+#   Copyright (C) 2015  Made to Order Software Corporation
 #
-#      This library is free software; you can redistribute it and/or
-#      modify it under the terms of the GNU Lesser General Public
-#      License as published by the Free Software Foundation; either
-#      version 2 of the License, or (at your option) any later version.
+#   This library is free software; you can redistribute it and/or
+#   modify it under the terms of the GNU Lesser General Public
+#   License as published by the Free Software Foundation; either
+#   version 2 of the License, or (at your option) any later version.
 #
-#      This library is distributed in the hope that it will be useful,
-#      but WITHOUT ANY WARRANTY; without even the implied warranty of
-#      MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
-#      Lesser General Public License for more details.
+#   This library is distributed in the hope that it will be useful,
+#   but WITHOUT ANY WARRANTY; without even the implied warranty of
+#   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+#   Lesser General Public License for more details.
 #
-#      You should have received a copy of the GNU Lesser General Public
-#      License along with this library; if not, write to the Free Software
-#      Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA 02111-1307  USA
+#   You should have received a copy of the GNU Lesser General Public
+#   License along with this library; if not, write to the Free Software
+#   Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA 02111-1307  USA
 #
 
 SET(CATCH_FOUND "NO")
