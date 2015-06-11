20e7fcf7778d81f8f1ccb81c2e9a94ff5c2a32b5
AlexisWilke <alexis@m2osw.com>
2015-06-11 00:31:39 -0700

    SNAP-187: re-run pbuilder-dist in case it fails... because it could just be that Hash Sum Mismatch error which is recoverable.

--- snapCMakeModules/Modules/SnapPBuilder.sh
+++ snapCMakeModules/Modules/SnapPBuilder.sh
@@ -25,6 +25,14 @@ fi
 NAME=`dpkg-parsechangelog --show-field Source`
 VERSION=`dpkg-parsechangelog --show-field Version`
 
-pbuilder-dist $1 build ../${NAME}_${VERSION}.dsc
+if ! pbuilder-dist $1 build ../${NAME}_${VERSION}.dsc
+then
+    # once in a while we get an error, try again immediately because in
+    # most cases the second attempt is more than likely to succeed just
+    # fine; i.e. if we get an error such as:
+    # https://bugs.launchpad.net/pbuilderjenkins/+bug/1233775
+    # if the problem is with the source, it will just fail again...
+    pbuilder-dist $1 build ../${NAME}_${VERSION}.dsc
+fi
 
 # vim: ts=4 sw=4 et
