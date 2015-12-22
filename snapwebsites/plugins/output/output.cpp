19e292e816c534a746e6c782505969a792fb21ce
AlexisWilke <alexis@m2osw.com>
2015-12-22 02:29:39 -0800

    EX-1: Moved the .navigation setup from finball.css.

--- snapwebsites/plugins/output/output.cpp
+++ snapwebsites/plugins/output/output.cpp
@@ -166,7 +166,7 @@ int64_t output::do_update(int64_t last_updated)
 {
     SNAP_PLUGIN_UPDATE_INIT();
 
-    SNAP_PLUGIN_UPDATE(2015, 12, 20, 18, 16, 44, content_update);
+    SNAP_PLUGIN_UPDATE(2015, 12, 22, 2, 28, 44, content_update);
 
     SNAP_PLUGIN_UPDATE_EXIT();
 }
