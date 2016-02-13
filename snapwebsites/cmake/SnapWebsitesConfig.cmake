6be7a31338c5ccc5cc5e9cbe5cdbd7cc2a8c3f9b
AlexisWilke <alexis@m2osw.com>
2016-02-12 17:30:57 -0800

    SNAP-303: fixed the path CMAKE_INSTALL_PREFIX is better in this case than CMAKE_PREFIX_PATH.

--- snapwebsites/cmake/SnapWebsitesConfig.cmake
+++ snapwebsites/cmake/SnapWebsitesConfig.cmake
@@ -34,6 +34,7 @@ find_package_handle_standard_args( SnapWebsites DEFAULT_MSG SNAPWEBSITES_INCLUDE
 #
 set( DTD_SOURCE_PATH /usr/share/snapwebsites/dtd CACHE PATH "Default DTD source files." )
 #
+include( SnapCssLint    )
 include( SnapJsLint    )
 include( SnapXmlLint   )
 include( SnapZipLayout )
