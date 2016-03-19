5f801b14a0fca2b96edf2a5d73385bb42fd18d16
AlexisWilke <alexis@m2osw.com>
2016-03-19 01:51:38 -0700

    EX-92: Changed the usage of the mouse pointer (Cursor) over the new Dropped File widget to better match other widgets.

--- snapwebsites/plugins/editor/editor.cpp
+++ snapwebsites/plugins/editor/editor.cpp
@@ -442,7 +442,7 @@ int64_t editor::do_update(int64_t last_updated)
 {
     SNAP_PLUGIN_UPDATE_INIT();
 
-    SNAP_PLUGIN_UPDATE(2016, 3, 18, 20, 23, 56, content_update);
+    SNAP_PLUGIN_UPDATE(2016, 3, 19, 0, 59, 56, content_update);
 
     SNAP_PLUGIN_UPDATE_EXIT();
 }
