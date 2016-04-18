7a39061b83a19b63a24a451a8aa940e0213674ed
R. Douglas Barbieri <doug@dooglio.net>
2016-04-18 13:05:42 -0700

    Now we can build without dependencies using the "-nodeps" suffix.

--- snapCMakeModules/Modules/SnapBuildConfig.cmake
+++ snapCMakeModules/Modules/SnapBuildConfig.cmake
@@ -35,23 +35,21 @@ find_program( MAKE_DPUT_SCRIPT   SnapBuildDputPackage.sh       PATHS ${CMAKE_MOD
 find_program( INC_DEPS_SCRIPT    SnapBuildIncDeps.pl           PATHS ${CMAKE_MODULE_PATH} )
 find_program( PBUILDER_SCRIPT    SnapPBuilder.sh			   PATHS ${CMAKE_MODULE_PATH} )
 
-function( ConfigureMakeProject )
+function( ConfigureMakeProjectInternal )
 	set( options        USE_CONFIGURE_SCRIPT NOINC_DEBVERS )
-	set( oneValueArgs   PROJECT_NAME VERSION DISTFILE_PATH )
+	set( oneValueArgs   PROJECT_NAME TARGET_NAME DISTFILE_PATH )
 	set( multiValueArgs CONFIG_ARGS DEPENDS )
 	cmake_parse_arguments( ARG "${options}" "${oneValueArgs}" "${multiValueArgs}" ${ARGN} )
 	#
 	if( NOT ARG_PROJECT_NAME )
 		message( FATAL_ERROR "You must specify PROJECT_NAME!" )
 	endif()
-
-	if( ARG_VERSION )
-		set( SRC_DIR   ${CMAKE_BINARY_DIR}/${ARG_PROJECT_NAME}-${ARG_VERSION}                                    )
-		set( BUILD_DIR ${CMAKE_BINARY_DIR}/${ARG_PROJECT_NAME}-${ARG_VERSION}/${ARG_PROJECT_NAME}-${ARG_VERSION} )
-	else()
-		set( SRC_DIR   ${CMAKE_SOURCE_DIR}/${ARG_PROJECT_NAME} )
-		set( BUILD_DIR ${CMAKE_BINARY_DIR}/${ARG_PROJECT_NAME} )
+	if( NOT ARG_TARGET_NAME )
+		message( FATAL_ERROR "You must specify TARGET_NAME!" )
 	endif()
+
+	set( SRC_DIR        ${CMAKE_SOURCE_DIR}/${ARG_PROJECT_NAME} )
+	set( BUILD_DIR      ${CMAKE_BINARY_DIR}/${ARG_TARGET_NAME}  )
 	set( SNAP_DIST_DIR "${CMAKE_BINARY_DIR}/dist" CACHE PATH "Destination installation folder." )
 	if( ARG_DISTFILE_PATH )
 		set( RM_DIR ${SRC_DIR}   )
@@ -81,18 +79,18 @@ function( ConfigureMakeProject )
 	endif()
 
 	#set_property( GLOBAL PROPERTY ${ARG_PROJECT_NAME}_DEPENDS_LIST ${ARG_DEPENDS} )
-	add_custom_target( ${ARG_PROJECT_NAME}-depends DEPENDS ${ARG_DEPENDS} )
+	add_custom_target( ${ARG_TARGET_NAME}-depends DEPENDS ${ARG_DEPENDS} )
 
 	if( ARG_USE_CONFIGURE_SCRIPT )
 		set( CONFIGURE_TARGETS ${BUILD_DIR}/config.log  )
 		add_custom_command(
 			OUTPUT ${CONFIGURE_TARGETS}
 			COMMAND ${BUILD_DIR}/configure --prefix=${SNAP_DIST_DIR} ${ARG_CONFIG_ARGS}
-				1> ${BUILD_DIR}/${ARG_PROJECT_NAME}_configure.log
-				2> ${BUILD_DIR}/${ARG_PROJECT_NAME}_configure.err
-			DEPENDS ${ARG_PROJECT_NAME}-depends
+				1> ${BUILD_DIR}/configure.log
+				2> ${BUILD_DIR}/configure.err
+			DEPENDS ${ARG_TARGET_NAME}-depends
 			WORKING_DIRECTORY ${BUILD_DIR}
-			COMMENT "Running ${ARG_PROJECT_NAME} configure script..."
+			COMMENT "Running ${ARG_TARGET_NAME} configure script..."
 			)
 	else()
 		set( COMMAND_LIST
@@ -102,7 +100,7 @@ function( ConfigureMakeProject )
 				${ARG_CONFIG_ARGS}
 				${SRC_DIR}
 	    )
-		set( CMD_FILE ${BUILD_DIR}/${ARG_PROJECT_NAME}_configure.cmd )
+		set( CMD_FILE ${BUILD_DIR}/configure.cmd )
 		file( REMOVE ${CMD_FILE} )
 		file( APPEND ${CMD_FILE} "cd " ${BUILD_DIR} "\n" )
 		file( APPEND ${CMD_FILE} "rm -f CMakeCache.txt\n" )
@@ -114,34 +112,34 @@ function( ConfigureMakeProject )
 		add_custom_command(
 			OUTPUT ${CONFIGURE_TARGETS}
 			COMMAND ${COMMAND_LIST}
-				1> ${BUILD_DIR}/${ARG_PROJECT_NAME}_configure.log
-				2> ${BUILD_DIR}/${ARG_PROJECT_NAME}_configure.err
-			DEPENDS ${ARG_PROJECT_NAME}-depends
+				1> ${BUILD_DIR}/configure.log
+				2> ${BUILD_DIR}/configure.err
+			DEPENDS ${ARG_TARGET_NAME}-depends
 			WORKING_DIRECTORY ${BUILD_DIR}
-			COMMENT "Running ${ARG_PROJECT_NAME} CMake configuration..."
+			COMMENT "Running ${ARG_TARGET_NAME} CMake configuration..."
 			)
 	endif()
 
 	add_custom_target(
-		${ARG_PROJECT_NAME}-make
+		${ARG_TARGET_NAME}-make
 		COMMAND ${CMAKE_BUILD_TOOL}
-			1> ${BUILD_DIR}/${ARG_PROJECT_NAME}_make.log
-			2> ${BUILD_DIR}/${ARG_PROJECT_NAME}_make.err
+			1> ${BUILD_DIR}/make.log
+			2> ${BUILD_DIR}/make.err
 		DEPENDS ${CONFIGURE_TARGETS}
 		WORKING_DIRECTORY ${BUILD_DIR}
-		COMMENT "Building ${ARG_PROJECT_NAME}"
+		COMMENT "Building ${ARG_TARGET_NAME}"
 		)
 
 	add_custom_target(
-		${ARG_PROJECT_NAME}-install
+		${ARG_TARGET_NAME}-install
 		COMMAND ${CMAKE_BUILD_TOOL} install
-			1>> ${BUILD_DIR}/${ARG_PROJECT_NAME}_make.log
-			2>> ${BUILD_DIR}/${ARG_PROJECT_NAME}_make.err
-		DEPENDS ${ARG_PROJECT_NAME}-make
+			1>> ${BUILD_DIR}/make.log
+			2>> ${BUILD_DIR}/make.err
+		DEPENDS ${ARG_TARGET_NAME}-make
 		WORKING_DIRECTORY ${BUILD_DIR}
-		COMMENT "Installing ${ARG_PROJECT_NAME}"
+		COMMENT "Installing ${ARG_TARGET_NAME}"
 		)
-	
+
 	# RDB: Tue Jan 26 10:13:24 PST 2016
 	# Adding ability to take the DEBUILD_EMAIL var from the environment as a default
 	#
@@ -166,52 +164,99 @@ function( ConfigureMakeProject )
 			list( APPEND DPUT_DEPS     ${DEP}-dput     )
 		endforeach()
 		add_custom_target(
-			${ARG_PROJECT_NAME}-incdeps
-			COMMAND ${INC_DEPS_SCRIPT} ${CMAKE_SOURCE_DIR} ${ARG_PROJECT_NAME} ${ARG_DEPENDS}
+			${ARG_TARGET_NAME}-incdeps
+			COMMAND ${INC_DEPS_SCRIPT} ${CMAKE_SOURCE_DIR} ${ARG_TARGET_NAME} ${ARG_DEPENDS}
 			WORKING_DIRECTORY ${SRC_DIR}
-			COMMENT "Incrementing dependencies for debian package ${ARG_PROJECT_NAME}"
+			COMMENT "Incrementing dependencies for debian package ${ARG_TARGET_NAME}"
 			)
 	else()
-		add_custom_target( ${ARG_PROJECT_NAME}-incdeps )
+		add_custom_target( ${ARG_TARGET_NAME}-incdeps )
 	endif()
 	unset( NOINC_DEBVERS )
 	if( ARG_NOINC_DEBVERS )
 		set( NOINC_DEBVERS "--noinc" )
 	endif()
 	add_custom_target(
-		${ARG_PROJECT_NAME}-debuild
+		${ARG_TARGET_NAME}-debuild
 		COMMAND env DEBEMAIL="${EMAIL_ADDY}" ${MAKE_SOURCE_SCRIPT} ${NOINC_DEBVERS} ${DEBUILD_PLATFORM}
-			1> ${BUILD_DIR}/${ARG_PROJECT_NAME}_debuild.log
+			1> ${BUILD_DIR}/debuild.log
 		WORKING_DIRECTORY ${SRC_DIR}
-		DEPENDS ${ARG_PROJECT_NAME}-incdeps
-		COMMENT "Building debian source package for ${ARG_PROJECT_NAME}"
+		DEPENDS ${ARG_TARGET_NAME}-incdeps
+		COMMENT "Building debian source package for ${ARG_TARGET_NAME}"
 		)
 	add_custom_target(
-		${ARG_PROJECT_NAME}-dput
+		${ARG_TARGET_NAME}-dput
 		COMMAND ${MAKE_DPUT_SCRIPT}
-			1> ${BUILD_DIR}/${ARG_PROJECT_NAME}_dput.log
+			1> ${BUILD_DIR}/dput.log
 		DEPENDS ${DPUT_DEPS}
 		WORKING_DIRECTORY ${SRC_DIR}
-		COMMENT "Dputting debian package ${ARG_PROJECT_NAME} to launchpad."
+		COMMENT "Dputting debian package ${ARG_TARGET_NAME} to launchpad."
 		)
 	add_custom_target(
-		${ARG_PROJECT_NAME}-pbuilder
+		${ARG_TARGET_NAME}-pbuilder
 		COMMAND ${PBUILDER_SCRIPT} ${DEBUILD_PLATFORM} 
-			1> ${BUILD_DIR}/${ARG_PROJECT_NAME}_pbuilder.log
+			1> ${BUILD_DIR}/pbuilder.log
 		DEPENDS ${PBUILDER_DEPS}
 		WORKING_DIRECTORY ${SRC_DIR}
-		COMMENT "Building debian package ${ARG_PROJECT_NAME} with pbuilder-dist."
+		COMMENT "Building debian package ${ARG_TARGET_NAME} with pbuilder-dist."
 		)
 
 	add_custom_target(
-		${ARG_PROJECT_NAME}-clean
+		${ARG_TARGET_NAME}-clean
 		COMMAND rm -rf ${RM_DIR}
 		)
 
 	add_custom_target(
-		${ARG_PROJECT_NAME}
-		DEPENDS ${ARG_PROJECT_NAME}-install
+		${ARG_TARGET_NAME}
+		DEPENDS ${ARG_TARGET_NAME}-install
 		)
+endfunction()
+
+function( ConfigureMakeProject )
+	set( options
+		USE_CONFIGURE_SCRIPT
+		NOINC_DEBVERS
+	)
+	set( oneValueArgs
+		PROJECT_NAME
+		DISTFILE_PATH
+	)
+	set( multiValueArgs
+		CONFIG_ARGS
+		DEPENDS
+	)
+	cmake_parse_arguments( ARG
+		"${options}"
+		"${oneValueArgs}"
+		"${multiValueArgs}"
+		${ARGN}
+	)
+
+	if( ARG_USE_CONFIGURE_SCRIPT )
+		set( CONF_SCRIPT_OPTION "USE_CONFIGURE_SCRIPT" )
+	endif()
+	if( ARG_NOINC_DEBVERS )
+		set( CONF_NOINC_OPTION "NOINC_DEBVERS" )
+	endif()
+
+	ConfigureMakeProjectInternal(
+		${CONF_SCRIPT_OPTION}
+		${CONF_NOINC_OPTION}
+		PROJECT_NAME  ${ARG_PROJECT_NAME}
+		TARGET_NAME   ${ARG_PROJECT_NAME}
+		DISTFILE_PATH ${ARG_DISTFILE_PATH}
+		CONFIG_ARGS   ${ARG_CONFIG_ARGS}
+		DEPENDS       ${ARG_DEPENDS}
+	)
+
+	ConfigureMakeProjectInternal(
+		${CONF_SCRIPT_OPTION}
+		${CONF_NOINC_OPTION}
+		PROJECT_NAME  ${ARG_PROJECT_NAME}
+		TARGET_NAME   ${ARG_PROJECT_NAME}-nodeps
+		DISTFILE_PATH ${ARG_DISTFILE_PATH}
+		CONFIG_ARGS   ${ARG_CONFIG_ARGS}
+	)
 
 	set_property( GLOBAL APPEND PROPERTY BUILD_TARGETS    ${ARG_PROJECT_NAME}          )
 	set_property( GLOBAL APPEND PROPERTY CLEAN_TARGETS    ${ARG_PROJECT_NAME}-clean    )
