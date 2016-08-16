106c211673e7cc9e113d2c15c2859ce8a05ccb1b
R. Douglas Barbieri <doug@m2osw.com>
2016-08-15 17:19:07 -0700

    SNAP-403: New vpn plugin handles client cert generation.
    The idea is that when you configure a server for the first time
    (postinst), the server keys will be created, but so will each client.
    Right now, this only works one time through. At some point, I need to
    add support to handle future client-adding.
    
    Also this code is completely untested and uncompiled.

--- snapwebsites/snapmanagercgi/plugins/vpn/vpn.cpp
+++ snapwebsites/snapmanagercgi/plugins/vpn/vpn.cpp
@@ -31,13 +31,13 @@
 #include "not_reached.h"
 #include "not_used.h"
 
+// Qt5
+//
+#include <QDir>
+
 // C++ lib
 //
 #include <fstream>
-
-// C lib
-//
-#include <glob.h>
 
 // last entry
 //
@@ -181,8 +181,6 @@ void vpn::bootstrap(snap_child * snap)
  */
 void vpn::on_retrieve_status(snap_manager::server_status & server_status)
 {
-    NOTUSED(server_status);
-
     if(f_snap->stop_now_prima())
     {
         return;
@@ -197,117 +195,57 @@ void vpn::on_retrieve_status(snap_manager::server_status & server_status)
         return;
     }
 
-#if 0
     // we want one field per user on the system, at this point we
     // assume that the system does not have hundreds of users since
     // only a few admins should be permitted on those computers
     // anyway...
     //
-    glob_t dir = glob_t();
-    int const r(glob(
-                  "/home/*"
-                , GLOB_NOESCAPE
-                , glob_error_callback
-                , &dir));
-    std::shared_ptr<glob_t> ai(&dir, glob_deleter);
 
-    if(r != 0)
-    {
-        // do nothing when errors occur
-        //
-        switch(r)
-        {
-        case GLOB_NOSPACE:
-            SNAP_LOG_ERROR("vpn.cpp: glob() did not have enough memory to alllocate its buffers.");
-            break;
-
-        case GLOB_ABORTED:
-            SNAP_LOG_ERROR("vpn.cpp: glob() was aborted after a read error.");
-            break;
-
-        case GLOB_NOMATCH:
-            SNAP_LOG_ERROR("vpn.cpp: glob() could not find any users on this computer.");
-            break;
-
-        default:
-            SNAP_LOG_ERROR("vpn.cpp: unknown glob() error code: ")(r)(".");
-            break;
-
-        }
-
-        // do not create any fields on error
-        return;
-    }
-
-    // check each user
-    // (TBD: how to "blacklist" some users so they do not appear here?)
+    // NOTE: TODO: Replace glob() with QDir.
     //
-    for(size_t idx(0); idx < dir.gl_pathc; ++idx)
-    {
-        // TODO: replace the direct handling of the file with a file_content object
-        //
-        std::string const home_path(dir.gl_pathv[idx]);
-        std::string const user_name(home_path.substr(6));
-        std::string const authorized_keys_path(home_path + "/.vpn/authorized_keys");
-        std::ifstream authorized_keys_in;
-        authorized_keys_in.open(authorized_keys_path, std::ios::in | std::ios::binary);
-        if(authorized_keys_in.is_open())
-        {
-            authorized_keys_in.seekg(0, std::ios::end);
-            std::ifstream::pos_type const size(authorized_keys_in.tellg());
-            authorized_keys_in.seekg(0, std::ios::beg);
-            std::string key;
-            key.resize(size, '?');
-            authorized_keys_in.read(&key[0], size);
-            if(!authorized_keys_in.fail()) // note: eof() will be true so good() will return false
-            {
-                // create a field for this one, it worked
-                //
-                snap_manager::status_t const rsa_key(
-                                  snap_manager::status_t::state_t::STATUS_STATE_INFO
-                                , get_plugin_name()
-                                , QString::fromUtf8(("authorized_keys::" + user_name).c_str())
-                                , QString::fromUtf8(key.c_str()));
-                server_status.set_field(rsa_key);
-            }
-            else
-            {
-                SNAP_LOG_DEBUG("could not read \"")(authorized_keys_path)("\" file for user \"")(user_name)("\".");
+    QStringList exts    = {"ovpn"}; //{"key","crt","ovpn"};
+    QStringList filters;
+    for( auto const &ext : ext ) { filters << QString("*.%1").arg(ext); }
 
-                // create an error field which is not editable
-                //
-                snap_manager::status_t const rsa_key(
-                                  snap_manager::status_t::state_t::STATUS_STATE_ERROR
-                                , get_plugin_name()
-                                , QString::fromUtf8(("authorized_keys::" + user_name).c_str())
-                                , QString());
-                server_status.set_field(rsa_key);
-            }
-        }
-        else
+    QDir keys_path;
+    keys_path.setPath( "/etc/openvpn/easy_rsa/keys/" );
+    keys_path.setNameFilters( filters );
+    keys_path.setSorting( QDir::Name );
+    keys_path.setFilter( QDir::Files );
+
+    for( QFileInfo const &info : keys_path.entryInfoList() )
+    {
+        // create a field for this one, it worked
+        //
+        for( auto const &ext : exts )
         {
-            // no authorized_keys file for that user
-            // create an empty field so one can add that file
-            //
-            snap_manager::status_t const rsa_key(
+            QFile file( QString("%1/%2.%3")
+                .arg(info.path())
+                .arg(info.baseName())
+                .arg(ext)
+                );
+            if( !file.open( QIODevice::ReadOnly| QIODevice::Text ) )
+            {
+                throw vpn_exception( QString("Cannot open '%1' for reading!").arg(info.filePath()) );
+            }
+
+            QTextStream in(&file);
+            snap_manager::status_t const ctl(
                               snap_manager::status_t::state_t::STATUS_STATE_INFO
                             , get_plugin_name()
-                            , QString::fromUtf8(("authorized_keys::" + user_name).c_str())
-                            , QString());
-            server_status.set_field(rsa_key);
+                            , QString("%1 %2").arg(info.baseName()).arg(ext)
+                            , in.readAll();
+                            );
+            server_status.set_field(ctl);
         }
     }
-#endif
 }
 
 
 bool vpn::is_installed()
 {
     // for now we just check whether the executable is here, this is
-    // faster than checking whether the package is installed and
-    // should be enough proof that the server is installed and
-    // running... and thus offer the editing of
-    // /home/*/.vpn/authorized_keys files
+    // faster than checking whether the package is installed.
     //
     return access("/usr/sbin/openvpn", R_OK | X_OK) == 0;
 }
@@ -334,57 +272,37 @@ bool vpn::display_value ( QDomElement parent
                         , snap::snap_uri const & uri
                         )
 {
-    NOTUSED(parent);
-    NOTUSED(s);
-    NOTUSED(uri);
-#if 0
     QDomDocument doc(parent.ownerDocument());
 
-    if(s.get_field_name().startsWith("authorized_keys::"))
+    // in case of an error, we do not let the user do anything
+    // so let the default behavior do its thing, it will show the
+    // field in a non-editable manner
+    //
+    if(s.get_state() == snap_manager::status_t::state_t::STATUS_STATE_ERROR)
     {
-        // in case of an error, we do not let the user do anything
-        // so let the default behavior do its thing, it will show the
-        // field in a non-editable manner
-        //
-        if(s.get_state() == snap_manager::status_t::state_t::STATUS_STATE_ERROR)
-        {
-            return false;
-        }
-
-        // the list of authorized_keys files
-        //
-        snap_manager::form f(
-                  get_plugin_name()
-                , s.get_field_name()
-                ,   snap_manager::form::FORM_BUTTON_RESET
-                  | snap_manager::form::FORM_BUTTON_RESTORE_DEFAULT
-                  | snap_manager::form::FORM_BUTTON_SAVE
-                );
-
-        QString const user_name(s.get_field_name().mid(17));
-        snap_manager::widget_text::pointer_t field(std::make_shared<snap_manager::widget_text>(
-                          "Authorized keys for \"" + user_name + "\""
-                        , s.get_field_name()
-                        , s.get_value()
-                        , "Enter your authorized_keys file in this field and click Save."
-                          " Then you will have access to this server via vpn. Use the"
-                          " \"Restore Default\" button to remove the file from this server."
-                        ));
-        f.add_widget(field);
-
-        f.generate(parent, uri);
-
-        return true;
+        return false;
     }
-#endif
 
-    return false;
+    // the list of client config files
+    //
+    snap_manager::form f(
+            get_plugin_name()
+            , s.get_field_name()
+            , snap_manager::form::FORM_BUTTON_REFRESH
+            );
+    snap_manager::widget_text::pointer_t field(std::make_shared<snap_manager::widget_text>(
+                "Paste this into the client system to activate."
+                , s.get_field_name()
+                , s.get_value()
+                , "This data will not be changed if you alter it."
+                ));
+    f.add_widget(field);
+    f.generate(parent, uri);
+    return true;
 }
 
 
-/** \brief Save 'new_value' in field 'field_name'.
- *
- * This function saves 'new_value' in 'field_name'.
+/** \brief Does nothing
  *
  * \param[in] button_name  The name of the button the user clicked.
  * \param[in] field_name  The name of the field to update.
@@ -407,71 +325,6 @@ bool vpn::apply_setting ( QString const & button_name
     NOTUSED(new_value);
     NOTUSED(old_or_installation_value);
     NOTUSED(affected_services);
-#if 0
-
-    // we support Save and Restore Default of the authorized_keys file
-    //
-    if(field_name.startsWith("authorized_keys::"))
-    {
-        // generate the path to the authorized_keys file
-        QString const user_name(field_name.mid(17));
-        std::string authorized_keys_path("/home/");
-        authorized_keys_path += user_name.toUtf8().data();
-        std::string const vpn_path(authorized_keys_path + "/.vpn");
-        authorized_keys_path += "/.vpn/authorized_keys";
-
-        // first check whether the user asked to restore the defaults
-        //
-        if(button_name == "restore_default")
-        {
-            // "Restore Default" means deleting the file (i.e. no more VPN
-            // access although we do not yet break existing connection which
-            // we certainly should do too...)
-            //
-            unlink(authorized_keys_path.c_str());
-            return true;
-        }
-
-        // next make sure the .vpn directory exists, if not create it
-        // as expected by vpn
-        //
-        struct stat s;
-        if(stat(vpn_path.c_str(), &s) != 0)
-        {
-            QString const q_vpn_path(QString::fromUtf8(vpn_path.c_str()));
-            if(mkdir_p(q_vpn_path, false) != 0)
-            {
-                SNAP_LOG_ERROR("we could not create the .vpn directory \"")(vpn_path)("\"");
-                return false;
-            }
-            chmod(vpn_path.c_str(), S_IRWXU); // 0700
-            chownnm(q_vpn_path, user_name, user_name);
-        }
-
-        if(button_name == "save")
-        {
-            // TODO: replace the direct handling of the file with a file_content object
-            //
-            std::ofstream authorized_keys_out;
-            authorized_keys_out.open(authorized_keys_path, std::ios::out | std::ios::trunc | std::ios::binary);
-            if(authorized_keys_out.is_open())
-            {
-                authorized_keys_out << new_value.trimmed().toUtf8().data() << std::endl;
-
-                chmod(authorized_keys_path.c_str(), S_IRUSR | S_IWUSR);
-
-                // WARNING: we would need to get the default name of the
-                // user main group instead of assuming it is his name
-                //
-                chownnm(QString::fromUtf8(authorized_keys_path.c_str()), user_name, user_name);
-                return true;
-            }
-
-            SNAP_LOG_ERROR("we could not open authorized_keys file \"")(authorized_keys_path)("\"");
-        }
-    }
-
-#endif
     return false;
 }
 
