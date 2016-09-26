6cc9b673b535fa8c348374f00927c41af9f32480
AlexisWilke <alexis@m2osw.com>
2016-09-26 09:07:50 -0700

    SNAP-110: Replaced a couple of references to snapinit.conf with snapcommunicator.conf.

--- snapwebsites/libsnapwebsites/src/snapwebsites.cpp
+++ snapwebsites/libsnapwebsites/src/snapwebsites.cpp
@@ -379,7 +379,7 @@ namespace
             advgetopt::getopt::GETOPT_FLAG_ENVIRONMENT_VARIABLE,
             "server-name",
             nullptr,
-            "The name of the server that is going to run this instance of snapserver (defined in /etc/snapwebsites/snapinit.conf), this parameter is required.",
+            "The name of the server that is going to run this instance of snapserver (defined in /etc/snapwebsites/snapcommunicator.conf), this parameter is required.",
             advgetopt::getopt::argument_mode_t::required_argument
         },
         {
@@ -1247,7 +1247,7 @@ void server::verify_server_name(std::string & server_name)
         // more than two dots, the sub-sub-sub...sub-domain is
         // the FQDN
         //
-        SNAP_LOG_WARNING("Hostname \"")(std::string(server_name))("\" includes a dot character (.) which is not supported by snap. We assume that indicates the end of the name. If that is not what you expect, edit snapinit.conf and set the name as you want it in server_name=...");
+        SNAP_LOG_WARNING("Hostname \"")(std::string(server_name))("\" includes a dot character (.) which is not supported by snap. We assume that indicates the end of the name. If that is not what you expect, edit snapcommunicator.conf and set the name as you want it in server_name=...");
     }
 
 
