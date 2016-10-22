992f4190c048bdfb7097640b0cba006f09af6ad8
R. Douglas Barbieri <doug@dooglio.net>
2016-10-22 15:13:22 -0700

    SNAP-487: Added script which modifies logrotate conffile for apache2
    This script file is to be used for snapcgi and snapmanagercgi, any
    package that requires apache2 to be installed.

---
+++ snapwebsites/snapbase/scripts/snapapachelog.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+LOGROTFILE="/etc/logrotate.d/apache2"
+
+if [ -e "${LOGROTFILE}" ]
+then
+	if ! grep maxsize "${LOGROTFILE}" >&1 /dev/null
+	then
+		sed \
+			-e "s/rotate 14/rotate 91/" \
+			-e "s/}$/\tsu root adm\n\tmaxsize 10M\n}/"
+			${LOGROTFILE}
+	fi
+fi
