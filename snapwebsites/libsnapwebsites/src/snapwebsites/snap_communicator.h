faf79c555c9a9a36d0bc63a1e6ca9782cbf6e6df
AlexisWilke <alexis@m2osw.com>
2016-11-04 16:37:29 -0700

    SNAP-393: Added an implementation of the inotify in our snap_communicator environment: `snap_file_changed`.

--- snapwebsites/libsnapwebsites/src/snapwebsites/snap_communicator.h
+++ snapwebsites/libsnapwebsites/src/snapwebsites/snap_communicator.h
@@ -162,7 +162,7 @@ public:
         virtual bool                valid_socket() const;
 
         bool                        is_enabled() const;
-        void                        set_enable(bool enabled);
+        virtual void                set_enable(bool enabled);
 
         int                         get_priority() const;
         void                        set_priority(priority_t priority);
@@ -380,6 +380,104 @@ public:
         virtual void                process_message(snap_communicator_message const & message) = 0;
 
     private:
+    };
+
+    class snap_file_changed
+        : public snap_connection
+    {
+    public:
+        typedef std::shared_ptr<snap_file_changed>      pointer_t;
+        typedef uint32_t                                event_mask_t;
+
+        static event_mask_t const   SNAP_FILE_CHANGED_EVENT_NO_EVENTS  = 0x0000;
+
+        // bits added to watch_...() functions
+        //
+        static event_mask_t const   SNAP_FILE_CHANGED_EVENT_ATTRIBUTES = 0x0001;   // chmod, chown (timestamp, link count, user/group, etc.)
+        static event_mask_t const   SNAP_FILE_CHANGED_EVENT_READ       = 0x0002;   // read, execve
+        static event_mask_t const   SNAP_FILE_CHANGED_EVENT_WRITE      = 0x0004;   // write, truncate
+        static event_mask_t const   SNAP_FILE_CHANGED_EVENT_CREATED    = 0x0008;   // open & O_CREAT, rename, mkdir, link, symlink, bind
+        static event_mask_t const   SNAP_FILE_CHANGED_EVENT_DELETED    = 0x0010;   // unlink, rename
+        static event_mask_t const   SNAP_FILE_CHANGED_EVENT_ACCESS     = 0x0020;   // open, close
+
+        static event_mask_t const   SNAP_FILE_CHANGED_EVENT_IO         = SNAP_FILE_CHANGED_EVENT_READ
+                                                                       | SNAP_FILE_CHANGED_EVENT_WRITE;
+
+        static event_mask_t const   SNAP_FILE_CHANGED_EVENT_ALL        = SNAP_FILE_CHANGED_EVENT_ATTRIBUTES
+                                                                       | SNAP_FILE_CHANGED_EVENT_IO
+                                                                       | SNAP_FILE_CHANGED_EVENT_CREATED
+                                                                       | SNAP_FILE_CHANGED_EVENT_DELETED
+                                                                       | SNAP_FILE_CHANGED_EVENT_ACCESS;
+
+        // flags added in event_t objects
+        //
+        static event_mask_t const   SNAP_FILE_CHANGED_EVENT_DIRECTORY  = 0x1000;   // object is a directory
+        static event_mask_t const   SNAP_FILE_CHANGED_EVENT_GONE       = 0x2000;   // removed
+        static event_mask_t const   SNAP_FILE_CHANGED_EVENT_UNMOUNTED  = 0x4000;   // unmounted
+
+        class event_t
+        {
+        public:
+                                    event_t(std::string const & watched_path
+                                          , event_mask_t events
+                                          , std::string const & filename);
+
+            std::string const &     get_watched_path() const;
+            event_mask_t            get_events() const;
+            std::string const &     get_filename() const;
+
+            bool                    operator < (event_t const & rhs) const;
+
+        private:
+            std::string             f_watched_path;
+            event_mask_t            f_events = 0;
+            std::string             f_filename;
+        };
+
+                                    snap_file_changed();
+        virtual                     ~snap_file_changed() override;
+
+        void                        watch_file(std::string const & watched_path, event_mask_t const events);
+        void                        watch_symlink(std::string const & watched_path, event_mask_t const events);
+        void                        watch_directory(std::string const & watched_path, event_mask_t const events);
+
+        void                        stop_watch(std::string const & watched_path);
+
+        // snap_connection implementation
+        virtual bool                is_reader() const override;
+        virtual int                 get_socket() const override;
+        virtual void                set_enable(bool enabled);
+        virtual void                process_read() override;
+
+        // new callback
+        virtual void                process_event(event_t const & watch_event) = 0;
+
+    private:
+        // TODO: RAII would be great with an impl and a counter...
+        //       (i.e. we make copies at the moment.)
+        struct watch_t
+        {
+            typedef std::map<int, watch_t>          map_t;
+
+                                    watch_t();
+                                    watch_t(std::string const & watched_path, event_mask_t events, uint32_t add_flags);
+
+            void                    add_watch(int inotify);
+            void                    merge_watch(int inotify, event_mask_t const events);
+            void                    remove_watch(int inotify);
+
+            std::string             f_watched_path;
+            event_mask_t            f_events = SNAP_FILE_CHANGED_EVENT_NO_EVENTS;
+            uint32_t                f_mask = 0;
+            int                     f_watch = -1;
+        };
+
+        bool                        merge_watch(std::string const & watched_path, event_mask_t const events);
+        static uint32_t             events_to_mask(event_mask_t const events);
+        static event_mask_t         mask_to_events(uint32_t const mask);
+
+        int                         f_inotify;
+        watch_t::map_t              f_watches;
     };
 
     class snap_tcp_client_connection
