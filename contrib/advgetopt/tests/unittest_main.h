1083af01b30471ad5dc4c6b4415187e8f4a923c2
AlexisWilke <alexis@m2osw.com>
2017-01-04 22:44:46 -0800

    Updated date of copyright notices and fixed some of them too.

--- contrib/advgetopt/tests/unittest_main.h
+++ contrib/advgetopt/tests/unittest_main.h
@@ -1,5 +1,5 @@
 /*    unittest_main.h
- *    Copyright (C) 2013-2015  Made to Order Software Corporation
+ *    Copyright (C) 2006-2017  Made to Order Software Corporation
  *
  *    This program is free software; you can redistribute it and/or modify
  *    it under the terms of the GNU General Public License as published by
