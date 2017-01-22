687680b917726bb3d18b2c4af63acb79a2a992ba
AlexisWilke <alexis@m2osw.com>
2017-01-21 16:10:33 -0800

    Bumped copyright notices to 2017.

--- snapwebsites/snapmanager/src/WebsiteModel.cpp
+++ snapwebsites/snapmanager/src/WebsiteModel.cpp
@@ -1,5 +1,5 @@
 //===============================================================================
-// Copyright (c) 2005-2016 by Made to Order Software Corporation
+// Copyright (c) 2015-2017 by Made to Order Software Corporation
 // 
 // All Rights Reserved.
 // 
