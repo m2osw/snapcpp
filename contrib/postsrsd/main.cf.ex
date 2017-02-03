f45168761d44aa106f08c1899affd3742199d8b9
R. Douglas Barbieri <doug@dooglio.net>
2017-02-03 00:11:42 -0800

    Removed postsrsd. The copy in ubuntu is good enough (v1.2).

--- contrib/postsrsd/main.cf.ex
+++
@@ -1,8 +0,0 @@
-# Add this fragment to your main.cf
-
-sender_canonical_maps = tcp:127.0.0.1:10001
-sender_canonical_classes = envelope_sender
-recipient_canonical_maps = tcp:127.0.0.1:10002
-recipient_canonical_classes = envelope_recipient,header_recipient
-
-
