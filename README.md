8cb7c6b4d50f91f940046ae4a1797bdb268dfb0a
AlexisWilke <alexis@m2osw.com>
2021-01-06 08:55:34 -0800

    Updated send-to-launchpad: support multiple distros.
    Updated the README.md with more details on how to compile everything.

--- README.md
+++ README.md
@@ -608,4 +608,6 @@ Submit bug reports and patches on
 [github](https://github.com/m2osw/snapcpp/issues).
 
 
+vim: ts=4 sw=4 et
+
 _This file is part of the [snapcpp project](https://snapwebsites.org/)._
--- bin/send-to-launchpad.sh
+++ bin/send-to-launchpad.sh
@@ -1,5 +1,7 @@
 #!/bin/sh -e
 
+DISTRIBUTIONS="xenial bionic focal"
+
 DIR=`pwd`
 BASENAME=`basename $DIR`
 if test "$BASENAME" = "snapwebsites"
@@ -10,7 +12,7 @@ fi
 if test "$1" = "-h" -o "$1" = "--help"
 then
     echo
-    echo "Usage: $0 [project-name]"
+    echo "Usage: $0 [project-name] [signature-email]"
     echo
     echo "  Where project-name defaults to \"snapwebsites\""
     echo "  and it can be set to any one of our contrib as well"
@@ -27,6 +29,13 @@ then
     MODULE="$1"
 else
     MODULE=snapwebsites
+fi
+
+if test -n "$2"
+then
+    DEBEMAIL="$2"
+else
+    DEBEMAIL="Build Server <build@m2osw.com>"
 fi
 
 TMP=../tmp
@@ -53,31 +62,70 @@ case $MODULE in
 esac
 
 # Verify that version is 4 numbers separated by 3 periods
-# The "~xenial" part has to be updated for each platform
-# (add (xenial|<another>|<another>|...) to support multiple versions.)
+# The "~<name>" part was removed, we should not ever need it
 #
 VERSION=`dpkg-parsechangelog -S version`
 VALID=`echo "$VERSION" | sed -e 's/[0-9]\+\.[0-9]\+\.[0-9]\+\.[0-9]\+~[a-z]\+//'`
 if test -n "$VALID"
 then
-    echo "error: invalid version \"$VERSION\": must be 4 numbers separated by periods."
+    echo "error: invalid version \"$VERSION\": must be 4 numbers separated by periods and followed by a distribution name."
     exit 1
 fi
 
-debuild -S -d -sa
+# Remove the distro from the version
+VERSION_NUMBER=`echo "$VERSION" | sed -e 's/~[a-z]\+//'`
 
 SOURCE_DIR=`pwd`
 
-# To send the source to Launchpad, we need to be at the same level as those
-# files
-cd ..
+if ! git diff-index --quiet HEAD --
+then
+    git status
+    echo "***"
+    echo "*** error: you have uncommited changes."
+    echo "***"
+    echo "*** error: this script makes changes to the debian/changelog"
+    echo "*** error: and debian/control files and we don't want you to lose"
+    echo "*** error: any data; please cd to ${SOURCE_DIR} and git commit"
+    echo "*** error: and then try again. Thank you."
+    echo "***"
+    exit 1
+fi
 
-dput ppa:snapcpp/ppa ${MODULE}_${VERSION}_source.changes
+trap 'restore_files' EXIT
 
-# Get rid of those files from our source tree
-mkdir -p $TMP/sources
-mv ${MODULE}_${VERSION}* $TMP/sources/.
+restore_files() {
+    # Restore the files we just changed so it doesn't look like we need a new
+    # commit and that a different distro is selected
+    #
+    git checkout ${SOURCE_DIR}/debian/changelog ${SOURCE_DIR}/debian/control
+}
 
-rm -f ${SOURCE_DIR}/debian/files
+for distro in ${DISTRIBUTIONS}
+do
+    # Before we generate the output, we need to fix the distribution name
+    # in each version found in the control file and the first line of the
+    # changelog
+
+    sed -i -e "s/\([0-9]\+\.[0-9]\+\.[0-9]\+\.[0-9]\+\)~[a-z]\+/\1~${distro}/g" debian/control
+    sed -i -e "1 s/\([0-9]\+\.[0-9]\+\.[0-9]\+\.[0-9]\+\)~[a-z]\+/\1~${distro}/" \
+            -e "1 s/ [a-z]\+;/ ${distro};/" debian/changelog
+
+    debuild -S -d -sa -m"${DEBEMAIL}"
+
+    # To send the source to Launchpad, we need to be at the same level as those
+    # files
+    cd ..
+
+    dput ppa:snapcpp/ppa ${MODULE}_${VERSION_NUMBER}~${distro}_source.changes
+
+    # Get rid of those files from our source tree
+    mkdir -p $TMP/sources
+    mv ${MODULE}_${VERSION_NUMBER}* $TMP/sources/.
+
+    rm -f ${SOURCE_DIR}/debian/files
+
+    cd ${SOURCE_DIR}
+done
+
 
 # vim: ts=4 sw=4 et
